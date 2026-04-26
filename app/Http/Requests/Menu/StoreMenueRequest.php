00a30d6a7081ab85cd84d2ea5d06a75d83c65b09
Wildan-AhmadF <codedbywil@gmail.com>
2026-04-26 13:12:27 +0700

    feat : buat request menu

---
+++ app/Http/Requests/Menu/StoreMenueRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests\Menu;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMenuRequest extends FormRequest
+{
+    // mengizinkan semua user melakukan request
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    // validasi data yang masuk
+    public function rules(): array
+    {
+        return [
+            'name'      => ['required', 'string', 'max:225', 'unique:menus,name'],
+            'slug'      => ['required', 'string', 'max:255', 'unique:menus,slug'],
+            'url'       => ['nullable', 'string', 'max:255', 'unique:menus,url'],
+            'parent_id' => ['nullable', 'exists:menus,id'],
+            'order'     => ['required', 'integer'],
+            'icon'      => ['nullable', 'string', 'max:255'],
+        ];
+    }
+}
