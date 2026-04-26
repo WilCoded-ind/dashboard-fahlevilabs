73b31f6ab2b9dab9b7733d3b8d46d3bca078648b
Wildan-AhmadF <codedbywil@gmail.com>
2026-04-26 13:40:01 +0700

    feat : buat middleware untuk cek permission

---
+++ app/Http/Middleware/CheckPermission.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Menu;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckPermission
+{
+    public function handle(Request $request, Closure $next, string $permission = 'can_view'): Response {
+        $user = $request->user();
+
+        if (!$user) {
+            return redirect('/not-found');
+        }
+
+        // ambil path saat ini
+        $currentPath = '/' . $request->path();
+
+        // cari menu berdasarkan url
+        $menu = Menu::where('url', $currentPath)->first();
+
+        // kalau menu tidak ditemukan maka, lewati
+        if (!$menu) {
+            return $next ($request);
+        }
+
+        // cek permission user berdasarkan role
+        $permission = $user->role->permissions()
+            ->where('menu_id', $menu->id)
+            ->first();
+ 
+        if (!$permission || !$permission->$permission) {
+            return redirect('/not-found');
+        }
+
+        return $next($request);
+    }
+}
